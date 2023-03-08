050b4adb4bee870480426e4a73bc9c4e93acffcd
dboyliao <qmalliao@gmail.com>
2023-03-08 20:35:33 +0800

    add __version__

--- setup.py
+++ setup.py
@@ -1,8 +1,22 @@
+from pathlib import Path
+
 from setuptools import find_packages, setup
+
+
+def _get_version():
+    version_file_path = Path(__file__).parent / "pipenv_d" / "_version.py"
+    if not version_file_path.exists():
+        print(f"WARN: no {version_file_path} found, using 0.0.0 as __version__")
+        return "0.0.0"
+    ns = {}
+    with version_file_path.open("r") as fid:
+        exec(fid.read(), {}, ns)
+    return ns["__version__"]
+
 
 setup(
     name="pipenv-d",
-    version="0.0.1",
+    version=_get_version(),
     packages=find_packages("."),
     install_requires=["pipenv"],
     author="dboyliao",
