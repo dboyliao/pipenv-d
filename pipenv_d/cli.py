ffdad6f3b5f5e8852b3243aa135cc64eaa3baf02
dboyliao <qmalliao@gmail.com>
2023-03-15 15:32:31 +0800

    handle keyboard interruption

--- pipenv_d/cli.py
+++ pipenv_d/cli.py
@@ -16,18 +16,22 @@ def _run_pipenv():
 
 
 def cli():
-    lockfile_path = get_lock_file_path(LOCKFILE_DIR)
-    active_lockfile = Path("Pipfile.lock")
-    if lockfile_path.exists():
-        with active_lockfile.open("w", encoding="utf8") as out_fid, lockfile_path.open(
-            "r", encoding="utf8"
-        ) as in_fid:
-            out_fid.write(in_fid.read())
-    ret_code = _run_pipenv()
-    if active_lockfile.exists():
-        with lockfile_path.open("w", encoding="utf8") as out_fid, active_lockfile.open(
-            "r", encoding="utf8"
-        ) as in_fid:
-            out_fid.write(in_fid.read())
-        active_lockfile.unlink()
-    return ret_code
+    try:
+        lockfile_path = get_lock_file_path(LOCKFILE_DIR)
+        active_lockfile = Path("Pipfile.lock")
+        if lockfile_path.exists():
+            with active_lockfile.open(
+                "w", encoding="utf8"
+            ) as out_fid, lockfile_path.open("r", encoding="utf8") as in_fid:
+                out_fid.write(in_fid.read())
+        ret_code = _run_pipenv()
+        if active_lockfile.exists():
+            with lockfile_path.open(
+                "w", encoding="utf8"
+            ) as out_fid, active_lockfile.open("r", encoding="utf8") as in_fid:
+                out_fid.write(in_fid.read())
+            active_lockfile.unlink()
+        return ret_code
+    except KeyboardInterrupt:
+        print("interrupted by user...")
+        return 1
